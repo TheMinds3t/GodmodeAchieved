9386cd64384731d3f79c272e7dbeca921f3f5f09
Ashton Schultz <42127046+TheMinds3t@users.noreply.github.com>
2025-12-23 16:24:51 -0600

    Update intestines.lua

--- godmode/scripts/definitions/stages/intestines.lua
+++ godmode/scripts/definitions/stages/intestines.lua
@@ -15,7 +15,7 @@ stage.graphics = {
 	boss_spot = "godmode/gfx/ui/stage/"..stage_prefix.."player_spot.png",
 
 	backdrop_gfx = {
-        Walls = {"1","2","3","4","5","6"},
+        Walls = {"1","2","3","4","5"},
         -- Walls = {"6"},
         NFloors = {"nfloor"},
         LFloors = {"lfloor"},
